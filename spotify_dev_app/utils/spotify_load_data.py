849276070701c72bb632856b9bfe0637bf584dae
ArianeDlns <ariane.dalens@student.ecp.fr>
2022-11-15 21:37:41 +0100

    [refacto] black files

--- spotify_dev_app/utils/spotify_load_data.py
+++ spotify_dev_app/utils/spotify_load_data.py
@@ -8,9 +8,11 @@ def getCollaborativePlaylists(sp, user: str):
     """
     collab_dict = {}
     playlists = sp.user_playlists(user)
-    for playlist in playlists['items']:
-        if playlist['collaborative'] or (playlist['owner']['id'] != user and '+' in playlist['name']):
-            collab_dict[playlist['name']] = playlist['id']
+    for playlist in playlists["items"]:
+        if playlist["collaborative"] or (
+            playlist["owner"]["id"] != user and "+" in playlist["name"]
+        ):
+            collab_dict[playlist["name"]] = playlist["id"]
     return collab_dict
 
 
@@ -25,9 +27,9 @@ def getTrackIDs(sp, user: str, playlist_id: str):
     """
     track_infos = []
     playlist = sp.user_playlist(user, playlist_id)
-    for idx, item in enumerate(playlist['tracks']['items']):
-        track = item['track']
-        track_infos.append([track['id'], idx, item['added_by']['id']])
+    for idx, item in enumerate(playlist["tracks"]["items"]):
+        track = item["track"]
+        track_infos.append([track["id"], idx, item["added_by"]["id"]])
     return track_infos
 
 
@@ -43,34 +45,49 @@ def getTrackFeatures(sp, id: str):
     features = sp.audio_features(id)
 
     # meta
-    name = meta['name']
-    album = meta['album']['name']
-    artist = meta['album']['artists'][0]['name']
-    release_date = meta['album']['release_date']
-    length = meta['duration_ms']
-    popularity = meta['popularity']
+    name = meta["name"]
+    album = meta["album"]["name"]
+    artist = meta["album"]["artists"][0]["name"]
+    release_date = meta["album"]["release_date"]
+    length = meta["duration_ms"]
+    popularity = meta["popularity"]
 
     # features
-    acousticness = features[0]['acousticness']
-    danceability = features[0]['danceability']
-    energy = features[0]['energy']
-    instrumentalness = features[0]['instrumentalness']
-    liveness = features[0]['liveness']
-    loudness = features[0]['loudness']
-    speechiness = features[0]['speechiness']
-    tempo = features[0]['tempo']
-    valence = features[0]['valence']
-    time_signature = features[0]['time_signature']
-    key = features[0]['key']
-    mode = features[0]['mode']
-    uri = features[0]['uri']
+    acousticness = features[0]["acousticness"]
+    danceability = features[0]["danceability"]
+    energy = features[0]["energy"]
+    instrumentalness = features[0]["instrumentalness"]
+    liveness = features[0]["liveness"]
+    loudness = features[0]["loudness"]
+    speechiness = features[0]["speechiness"]
+    tempo = features[0]["tempo"]
+    valence = features[0]["valence"]
+    time_signature = features[0]["time_signature"]
+    key = features[0]["key"]
+    mode = features[0]["mode"]
+    uri = features[0]["uri"]
 
-    track = [name, album, artist, release_date,
-             length, popularity, acousticness,
-             danceability, energy, instrumentalness,
-             liveness, loudness, speechiness, tempo,
-             valence, time_signature,
-             key, mode, uri]
+    track = [
+        name,
+        album,
+        artist,
+        release_date,
+        length,
+        popularity,
+        acousticness,
+        danceability,
+        energy,
+        instrumentalness,
+        liveness,
+        loudness,
+        speechiness,
+        tempo,
+        valence,
+        time_signature,
+        key,
+        mode,
+        uri,
+    ]
     return track
 
 
@@ -85,13 +102,33 @@ def createPlaylistInfo(sp, username: str, playlist_id: str):
         features_list.append([*features, *track_info[1:]])
 
     # Create a dataframe from the list of features
-    df = pd.DataFrame(features_list, columns=['name', 'album', 'artist', 'release_date',
-                                              'length', 'popularity', 'acousticness',
-                                              'danceability', 'energy', 'instrumentalness',
-                                              'liveness', 'loudness', 'speechiness', 'tempo',
-                                              'valence', 'time_signature',
-                                              'key', 'mode', 'uri', 'playlist_idx', 'added_by'])
+    df = pd.DataFrame(
+        features_list,
+        columns=[
+            "name",
+            "album",
+            "artist",
+            "release_date",
+            "length",
+            "popularity",
+            "acousticness",
+            "danceability",
+            "energy",
+            "instrumentalness",
+            "liveness",
+            "loudness",
+            "speechiness",
+            "tempo",
+            "valence",
+            "time_signature",
+            "key",
+            "mode",
+            "uri",
+            "playlist_idx",
+            "added_by",
+        ],
+    )
     # save to csv with timestamp
-    timestamp = pd.Timestamp.now().strftime('%Y%m%d')
-    df.to_csv(f'./data/{timestamp}_{playlist_id}.csv', index=False, encoding='utf-8')
+    timestamp = pd.Timestamp.now().strftime("%Y%m%d")
+    df.to_csv(f"./data/{timestamp}_{playlist_id}.csv", index=False, encoding="utf-8")
     return df
--- spotify_dev_app/utils/spotify_plots.py
+++ spotify_dev_app/utils/spotify_plots.py
@@ -4,27 +4,21 @@ import plotly.graph_objects as go
 from colormap import rgb2hex
 
 
-def createRadarChart(df: pd.DataFrame, attributes, user: str, color='blue') -> go.Figure:
+def createRadarChart(
+    df: pd.DataFrame, attributes, user: str, color="blue"
+) -> go.Figure:
     df = df.loc[user][attributes]
     fig = go.Figure()
-    fig.add_trace(go.Scatterpolar(
-        r=df.values,
-        theta=df.index.values,
-        fill='toself'
-    )
-    )
+    fig.add_trace(go.Scatterpolar(r=df.values, theta=df.index.values, fill="toself"))
     # update colors
-    fig.update_traces(fillcolor=rgb2hex(
-        color[0], color[1], color[2]), line_color=rgb2hex(color[0], color[1], color[2]))
-    fig.update_layout(
-        polar=dict(
-            radialaxis=dict(
-                visible=True,
-                range=[0, 1]
-            )),
-        showlegend=False
+    fig.update_traces(
+        fillcolor=rgb2hex(color[0], color[1], color[2]),
+        line_color=rgb2hex(color[0], color[1], color[2]),
     )
-    #fig.update_layout(
+    fig.update_layout(
+        polar=dict(radialaxis=dict(visible=True, range=[0, 1])), showlegend=False
+    )
+    # fig.update_layout(
     #    title={
     #        'text': f"<br>{user}</br> addition to the playlist",
     #        'y': 0.9,
